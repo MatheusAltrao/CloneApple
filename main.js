5d2a7e93a9857aa480d0bb005d8178baa431d8c2
matheus <matheusaltrao2@gmail.com>
2022-09-28 15:21:50 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const productImage = document.getElementById('product-image')
+
+const green = document.getElementById('green')
+const silver = document.getElementById('silver')
+const golden = document.getElementById('golden')
+const grafite = document.getElementById('grafite')
+const blue = document.getElementById('blue')
+
+const imagesPhoneArray = [
+  'img/iphone_green.jpg',
+  'img/iphone_silver.jpg',
+  'img/iphone_golden.jpg',
+  'img/iphone_grafite.jpg',
+  'img/iphone_blue.jpg'
+]
+
+
+green.addEventListener('click', () => {
+  productImage.src = imagesPhoneArray[0]
+})
+
+silver.addEventListener('click', () => {
+  productImage.src = imagesPhoneArray[1]
+})
+
+golden.addEventListener('click', () => {
+  productImage.src = imagesPhoneArray[2]
+})
+
+grafite.addEventListener('click', () => {
+  productImage.src = imagesPhoneArray[3]
+})
+
+blue.addEventListener('click', () => {
+  productImage.src = imagesPhoneArray[4]
+})
+
+
+
+var menu = document.getElementById('menu')
+var links = document.getElementById('links')
+
+menu.onclick = function () {
+  menu.classList.toggle('openmenu')
+  links.classList.toggle('active')
+}
+
