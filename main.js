a08561c0097942ef99996dd30e34be37a468121b
matheus <matheusaltrao2@gmail.com>
2022-10-16 18:41:07 -0400

    add function buttonChecked

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const productImage = document.getElementById('product-image')
 
+
 const green = document.getElementById('green')
 const silver = document.getElementById('silver')
 const golden = document.getElementById('golden')
@@ -15,24 +16,52 @@ const imagesPhoneArray = [
 ]
 
 
+let buttons = document.querySelectorAll('#listbutton li button')
+
+function removeChecked() {
+  let buttons = document.querySelectorAll('#listbutton li button')
+
+  buttons.forEach((ev) => {
+    ev.classList.remove('focus')
+  })
+}
+
+
 green.addEventListener('click', () => {
   productImage.src = imagesPhoneArray[0]
+
+  removeChecked()
+  buttons[0].classList.add('focus')
+
 })
 
 silver.addEventListener('click', () => {
   productImage.src = imagesPhoneArray[1]
+
+  removeChecked()
+  buttons[1].classList.add('focus')
 })
 
 golden.addEventListener('click', () => {
   productImage.src = imagesPhoneArray[2]
+
+  removeChecked()
+  buttons[2].classList.add('focus')
+
 })
 
 grafite.addEventListener('click', () => {
   productImage.src = imagesPhoneArray[3]
+
+  removeChecked()
+  buttons[3].classList.add('focus')
 })
 
 blue.addEventListener('click', () => {
   productImage.src = imagesPhoneArray[4]
+
+  removeChecked()
+  buttons[4].classList.add('focus')
 })
 
 
